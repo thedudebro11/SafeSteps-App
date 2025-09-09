cdc9cd2cba2ee5865e0170b4514414b4e3c39ec6
thedudebro11 <thedevguy0101@gmail.com>
2025-09-09 09:09:53 +0000

    fix: reset Babel config to Expo SDK 53 defaults

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,20 @@
 module.exports = function (api) {
   api.cache(true);
-  return { presets: ["babel-preset-expo"], plugins: ["nativewind/babel"] };
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      // TS path aliases (matches your tsconfig)
+      ['module-resolver', {
+        extensions: ['.tsx', '.ts', '.js', '.jsx', '.json'],
+        alias: { '@': './src', '@ui': './src/ui' },
+      }],
+
+      // If you use these packages, leave them; otherwise they’re harmless
+      'nativewind/babel',
+      'react-native-css-interop/babel',
+
+      // Reanimated must be last
+      'react-native-reanimated/plugin',
+    ],
+  };
 };
